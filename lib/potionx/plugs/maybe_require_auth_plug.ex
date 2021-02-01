66080ac839fe8607d79e6e349ddf254981d4a16f
Vince Roy <vincer0117@gmail.com>
2021-02-01 16:28:34 -0500

    try to refresh token in maybe_require_auth plug

--- lib/potionx/plugs/maybe_require_auth_plug.ex
+++ lib/potionx/plugs/maybe_require_auth_plug.ex
@@ -21,14 +21,38 @@ defmodule Potionx.Plug.MaybeRequireAuth do
   end
 
   defp maybe_redirect(nil, conn, config) do
-    cond do
-      Enum.member?(config.public_urls, conn.host) ->
+    pow_config = Pow.Plug.fetch_config(conn)
+
+    conn
+    |> Potionx.Plug.ApiAuth.renew(pow_config)
+    |> case do
+      {conn, nil} ->
+        cond do
+          Enum.member?(config.public_urls, conn.host) ->
+            conn
+          Enum.at(conn.path_info, 0) === "login" ->
+            conn
+          true ->
+            Phoenix.Controller.redirect(conn, to: config.login_path)
+            |> Conn.halt
+        end
+      {conn, _user} ->
         conn
-      Enum.at(conn.path_info, 0) === "login" ->
-        conn
-      true ->
-        Phoenix.Controller.redirect(conn, to: config.login_path)
-        |> Conn.halt
+        |> Potionx.Plug.ApiAuth.handle_cookies(
+          %{
+            access_token: conn.private[:api_access_token],
+            renewal_token: conn.private[:api_renewal_token]
+          },
+          pow_config
+        )
+        |> (fn conn ->
+          if Enum.at(conn.path_info, 0) === "login" do
+            Phoenix.Controller.redirect(conn, to: "/")
+            |> Conn.halt
+          else
+            conn
+          end
+        end).()
     end
   end
   defp maybe_redirect(_user, conn, _handler) do
