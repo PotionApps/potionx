c3b729d775bd3243d5419d24e250acc0d1914083
demchi <mike@potionapps.com>
2021-02-01 11:32:48 -0500

    static AdminFooter content to demo it in RouteAdmin

--- packages/ui/src/playground/RouteAdmin/RouteAdmin.tsx
+++ packages/ui/src/playground/RouteAdmin/RouteAdmin.tsx
@@ -1,14 +1,29 @@
-import { defineComponent } from "vue";
-import PotionLogo from '../../assets/potion-logo.svg'
+import { defineComponent, computed } from "vue";
 import AdminListLayout from "../../layouts/AdminListLayout/AdminListLayout";
+import { routeNames } from "../../playground/routeNames";
+import Bars from "../../assets/bars.svg";
 
 export default defineComponent({
   setup () {
 
+    const mobileBtns = computed(() => [
+      {
+        icon: Bars,
+        label: "Back",
+        to: {
+          name: routeNames.login
+        }
+      },
+      {
+        label: "New Entry",
+        to: {
+          name: routeNames.login
+        }
+      }
+    ])
+
     return () => <AdminListLayout
-      v-slots={{
-        logo: () => <img class="w-4" src={PotionLogo}/>
-      }}
+      adminFooterBtns={mobileBtns}
     >
       <h1 class="text-gray-800 text-2xl mb-8">admin</h1>
     </AdminListLayout>
