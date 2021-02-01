48fe1d987f758ca61c8ac1025fee9ca8168dadf4
Vince Roy <vincer0117@gmail.com>
2021-02-01 09:59:10 -0500

    export model table props from ui

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,6 @@
 import Login from './components/Login/Login'
 import LoginButton from './components/LoginButton/LoginButton'
 import LoginError from './components/LoginError/LoginError'
-import ModelTable from './components/ModelTable/ModelTable'
+import ModelTable, { ModelTableProps } from './components/ModelTable/ModelTable'
 
-export { Login, LoginButton, LoginError, ModelTable }
+export { Login, LoginButton, LoginError, ModelTable, ModelTableProps }
