e121dc3f22e8e5d57fb97ad4af8d545dbe58ec2a
Vince Roy <vincer0117@gmail.com>
2021-02-01 17:43:14 -0500

    add adminHeaderNav sync

--- installer/templates/potionx/frontend/admin/src/useAdminHeaderNav.ts
+++ installer/templates/potionx/frontend/admin/src/useAdminHeaderNav.ts
@@ -1,5 +1,6 @@
 import { AdminHeaderNavProps } from "@potionapps/ui";
 import { computed, Ref } from "vue";
+import { routeNames } from './routes/routeNames'
 
 const nav : AdminHeaderNavProps['nav'] = []
 
