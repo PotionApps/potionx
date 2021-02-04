42cfc1fef8cda4456a37c8ddfb7fd0f42374d45d
Vince Roy <vincer0117@gmail.com>
2021-02-04 12:14:21 -0500

    potionx_new 0.0.10

--- installer/mix.exs
+++ installer/mix.exs
@@ -6,7 +6,7 @@ end
 defmodule Potionx.New.MixProject do
   use Mix.Project
 
-  @version "0.0.9"
+  @version "0.0.10"
   @github_path "potionapps/potionx"
   @url "https://github.com/#{@github_path}"
 
