fdaa2b7f49b2dbcf27caf390e7d0bd29b0ca0fe5
Vince Roy <vincer0117@gmail.com>
2021-02-09 15:29:13 -0500

    capitalize field label by default

--- packages/forms/src/fields/FieldLabel.tsx
+++ packages/forms/src/fields/FieldLabel.tsx
@@ -2,7 +2,7 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   setup (_, ctx) {
-    return () => <label class="text-gray-700">
+    return () => <label class="capitalize text-gray-700">
       {ctx.slots.default && ctx.slots.default()}
     </label>
   }
