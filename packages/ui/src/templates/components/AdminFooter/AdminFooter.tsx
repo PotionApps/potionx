360d9c2ecd0e4cff2c45c5ec69dbd325075f912d
demchi <mike@potionapps.com>
2021-02-23 17:06:15 -0500

    Menu back button history fix

--- packages/ui/src/templates/components/AdminFooter/AdminFooter.tsx
+++ packages/ui/src/templates/components/AdminFooter/AdminFooter.tsx
@@ -1,7 +1,7 @@
+import Btn from "../Btn/Btn";
 import { defineComponent } from "vue";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import Btn from "../Btn/Btn";
-import { routeNames } from 'root/routes/routeNames'
+import { useRouter } from "vue-router"
 
 export interface PropsAdminFooter {
   hideMenu?: boolean
@@ -13,6 +13,8 @@ export default defineComponent({
     hideMenu: Boolean
   },
   setup (props, ctx) {
+    const router = useRouter()
+
     return () => {
       return (
         <div class="bg-gray-900 bottom-0 fixed flex h-14 s1050:hidden justify-end left-0 pl-2 py-2 shadow w-full z-99">
@@ -21,13 +23,7 @@ export default defineComponent({
             !props.hideMenu && <Btn
               class="bg-gray-700 focus:bg-gray-600 hover:bg-gray-600 flex flex-fit h-auto items-center justify-center mr-2 px-3 py-2 rounded text-gray-300 w-auto"
               icon={faBars}
-              to={
-                {
-                  query: {
-                    menu: "1"
-                  }
-                }
-              }
+              click={() => router.replace({query: {menu: "1"}})}
             />
           }
         </div>
