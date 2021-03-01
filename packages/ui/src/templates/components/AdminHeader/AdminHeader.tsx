858b658150777a318492bf37fb73471c5c0b971c
Vince Roy <vincer0117@gmail.com>
2021-02-28 19:53:09 -0500

    remove dependency on Wrapper in AdminHeader

--- packages/ui/src/templates/components/AdminHeader/AdminHeader.tsx
+++ packages/ui/src/templates/components/AdminHeader/AdminHeader.tsx
@@ -1,6 +1,5 @@
 import { defineComponent, PropType, Ref } from "vue";
 import { RouteLocationRaw } from "vue-router";
-import Wrapper from "../Wrapper/Wrapper";
 
 export interface AdminHeaderTabProps {
   label: string
@@ -20,14 +19,14 @@ export default defineComponent({
     return () => {
       return (
         <div class="bg-white border-b-1 border-gray-300 pt-3">
-          <Wrapper>
+          <div class="px-4 s1050:px-8 s1450:px-12">
             <div class="flex s1050m:flex-wrap items-center justify-between">
               <div class="flex flex-full items-center s1050:max-w-500 s1450:max-w-600 pb-3 pr-2">
                 {ctx.slots.default && ctx.slots.default()}
               </div>
               {ctx.slots.btns && <div class="flex">{ctx.slots.btns()}</div>}
             </div>
-          </Wrapper>
+          </div>
         </div>
       )
     }
