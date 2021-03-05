c4fbc5a97d8615978d35e9f52bb7c1e2baec7446
Vince Roy <vincer0117@gmail.com>
2021-03-04 21:55:23 -0500

    ui: loose pagination prop requirements

--- packages/ui/src/templates/components/Pagination/Pagination.tsx
+++ packages/ui/src/templates/components/Pagination/Pagination.tsx
@@ -1,14 +1,14 @@
-import { defineComponent, PropType } from "vue"
+import { defineComponent, PropType, computed } from "vue"
 import { faAngleDoubleLeft, faAngleDoubleRight, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@potionapps/utils';
 import BtnSecondary from "../Btn/BtnSecondary";
 
 export interface PaginationProps {
-  count: number
-  countBefore: number
+  count?: number
+  countBefore?: number
   goToFirst: () => void
   goToLast: () => void
-  limit: number
+  limit?: number
   next: () => void
   prev: () => void
 }
@@ -16,11 +16,9 @@ export interface PaginationProps {
 export default defineComponent({
   props: {
     count: {
-      required: true,
       type: Number
     },
     countBefore: {
-      required: true,
       type: Number
     },
     goToFirst: {
@@ -32,7 +30,6 @@ export default defineComponent({
       type: Function as PropType<PaginationProps['goToLast']>
     },
     limit: {
-      required: true,
       type: Number
     },
     next: {
@@ -46,6 +43,10 @@ export default defineComponent({
   },
   name: "Pagination",
   setup (props: PaginationProps) {
+    const hasNext = computed(() => {
+      return (props.countBefore || 0) + (props.limit || 0) === (props.count || 0)
+    })
+
     return () => {
       return (
         <div class="bg-white bottom-0 s1050m:bottom-8 flex items-center justify-center mt-4 s750:mt-8 pb-12 pt-2 sticky z-3">
@@ -66,19 +67,25 @@ export default defineComponent({
             label="Prev"
           />
           <span class={"font-bold text-gray-700 text-sm s450:text-base"}>
-            {props.countBefore}–{Math.min(props.countBefore + props.limit, props.count)} of {props.count}
+            {
+              !props.count && "-"
+            }
+            {
+              props.count && props.countBefore && props.limit &&
+              `${props.countBefore}–${Math.min(props.countBefore + props.limit, props.count)} of ${props.count}`
+            }
           </span>
           <BtnSecondary
             class="ml-4 mr-2"
             click={props.next}
-            disabled={props.countBefore + props.limit === props.count}
+            disabled={hasNext.value}
             icon={faAngleRight}
             label="Next"
             reverse={true}
           />
           <div
             class={
-              (props.countBefore + props.limit === props.count ? "opacity-40 pointer-events-none" : "opacity-80") +
+              (hasNext.value ? "opacity-40 pointer-events-none" : "opacity-80") +
               " cursor-pointer focus:opacity-100 hover:opacity-100 px-1 transition-opacity"
             }
             onClick={props.goToLast}
