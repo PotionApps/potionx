dcba890a1a62cae61f1c574cd4528e9f60c8dc1e
Vince Roy <vincer0117@gmail.com>
2021-03-25 19:50:34 -0400

    potionx: add changeset middleware test

---
+++ test/graphql/changeset_errors_middleware_test.exs
@@ -0,0 +1,40 @@
+defmodule Potionx.Auth.Test do
+  use Potionx.ConnCase
+  alias PotionxTest.Router
+
+  describe "Authentication test" do
+    setup do
+      {:ok, %{
+        secret_key_base: :crypto.strong_rand_bytes(64) |> Base.encode64 |> binary_part(0, 64)
+      }}
+    end
+
+    test "Should return ecto transaction errors", %{secret_key_base: secret_key_base} do
+      query = """
+        mutation {
+          changesetMiddleware {
+            errors
+            errorsFields {
+              field
+              message
+            }
+          }
+        }
+      """
+      conn(:post, "/graphql", %{variables: %{}, query: query})
+      |> Map.replace(:secret_key_base, secret_key_base)
+      |> Router.call(Router.init([]))
+      |> sent_resp
+      |> elem(2)
+      |> Jason.decode!
+      |> case do
+        %{"data" =>  %{"changesetMiddleware" => res}} ->
+          assert res["errorsFields"] === [
+            %{"field" => "email", "message" => "can't be blank"},
+            %{"field" => "name_first", "message" => "can't be blank"}
+          ]
+      end
+    end
+
+  end
+end
--- test/support/auth/test_schema.ex
+++ test/support/auth/test_schema.ex
@@ -1,12 +1,28 @@
 defmodule PotionxTest.Schema do
-  use Potionx.Schema
+  use Potionx.Schema, [
+    user_required_exceptions: [:session_renew, :sign_in_provider, :changeset_middleware]
+  ]
 
   import_types Potionx.Types
+
+  object :tx_mutation_result do
+    field :errors, list_of(:string)
+    field :errors_fields, list_of(:error)
+  end
 
   query do
   end
 
   mutation do
+    field :changeset_middleware, type: :tx_mutation_result do
+      resolve fn _, _, _ ->
+        {
+          :error,
+          Ecto.Changeset.cast(%PotionxTest.User{}, %{}, [])
+          |> Ecto.Changeset.validate_required([:name_first, :email])
+        }
+      end
+    end
     import_fields :auth_mutations
   end
   import_types PotionxTest.Mutations
