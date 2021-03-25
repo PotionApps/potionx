203657432d6a592dce8c90ccf26cf44c7dbdbf55
Vince Roy <vincer0117@gmail.com>
2021-03-25 10:51:59 -0400

    templates: add min-w-0 to admin main to prevent overflow issues

--- packages/templates/src/component/AdminMain/AdminMain.tsx
+++ packages/templates/src/component/AdminMain/AdminMain.tsx
@@ -5,7 +5,7 @@ export default defineComponent({
   setup (_, ctx) {
     return () => {
       return (
-        <div class="bg-white min-h-screen s1050m:pb-14">
+        <div class="bg-white min-h-screen s1050m:pb-14 min-w-0">
           {ctx.slots.default && ctx.slots.default()}
         </div>
       )
