cbfa73dabdbcb1280969af37dbba188d14bab844
Vince Roy <vincer0117@gmail.com>
2021-03-26 10:06:33 -0400

    templates: change user email to citext

--- packages/templates/src/project/potionx/priv/repo/migrations/{{ timestamp }}_create_users.exs
+++ packages/templates/src/project/potionx/priv/repo/migrations/{{ timestamp }}_create_users.exs
@@ -2,8 +2,9 @@ defmodule <%= appModule %>.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
+    execute "CREATE EXTENSION IF NOT EXISTS citext",  "DROP EXTENSION IF EXISTS citext"
     create table(:users) do
-      add :email, :string, null: false
+      add :email, :citext, null: false
       add :deleted_at, :utc_datetime
       add :name_first, :string
       add :name_last, :string
