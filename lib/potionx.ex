31fde78095e5706f23a4bd4853516ab2480cff79
Vince Roy <vincer0117@gmail.com>
2021-03-29 17:35:46 -0400

    potionx: remove unused function in application.ex

--- lib/potionx.ex
+++ lib/potionx.ex
@@ -2,17 +2,4 @@ defmodule Potionx do
   @moduledoc """
   Documentation for `Potionx`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Potionx.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
