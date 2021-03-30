46f1b71f91db64e5f0b780756dfe75c090e8cfe0
Vince Roy <vincer0117@gmail.com>
2021-03-30 13:28:21 -0400

    templates: disable phx live reload iframe

--- packages/templates/src/project/potionx/lib/{{ appName }}_web/endpoint.ex
+++ packages/templates/src/project/potionx/lib/{{ appName }}_web/endpoint.ex
@@ -30,8 +30,8 @@ defmodule <%= endpointModule %> do
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
+    # socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    # plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :<%= webAppName %>
   end
