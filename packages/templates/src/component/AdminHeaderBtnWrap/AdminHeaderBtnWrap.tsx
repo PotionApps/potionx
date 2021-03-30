38d8c1fc75f820eeae0fcc53b8bea3f461b3b338
Vince Roy <vincer0117@gmail.com>
2021-03-30 12:31:45 -0400

    templates: remove ambiguous hidden prop on AdminHeaderBtnWrap

--- packages/templates/src/component/AdminHeaderBtnWrap/AdminHeaderBtnWrap.tsx
+++ packages/templates/src/component/AdminHeaderBtnWrap/AdminHeaderBtnWrap.tsx
@@ -1,18 +1,11 @@
 import { defineComponent } from "vue";
 
-export interface AdminHeaderBtnWrapProps {
-  hidden?: boolean
-}
-
 export default defineComponent({
   name: "AdminHeaderBtnWrap",
-  props: {
-    hidden: Boolean
-  },
-  setup (props: AdminHeaderBtnWrapProps, ctx) {
+  setup (props, ctx) {
     return () => {
       return (
-        <div class={["mb-3 ml-3 first:ml-0", props.hidden && "s1050m:hidden"]}>
+        <div class={["mb-3 ml-3 first:ml-0"]}>
           {ctx.slots.default && ctx.slots.default()}
         </div>
       )
