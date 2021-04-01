d52c9410af90c0b01411e858c48c6670d7f8cf53
Vince Roy <vincer0117@gmail.com>
2021-03-31 20:59:14 -0400

    potionx: version 0.4.15

--- packages/templates/src/project/potionx/template.config.mjs
+++ packages/templates/src/project/potionx/template.config.mjs
@@ -111,7 +111,7 @@ export const getValues = async (context, prompt, initialValues) => {
   }
 
   const collected = await prompt(toCollect);
-  const potionx_version = "0.4.14";
+  const potionx_version = "0.4.15";
 
 
   const timestamp = new Date()
