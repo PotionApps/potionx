fdb9fda65d68ee3403441c69b31386b776266eae
Vince Roy <vincer0117@gmail.com>
2021-06-02 10:44:10 -0400

    potionx: 0.7.1

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Potionx.MixProject do
   def project do
     [
       app: :potionx,
-      version: "0.7.0",
+      version: "0.7.1",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
