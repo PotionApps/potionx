1f1f1ca6d93a38c7668f464c201eaf925f7eeac9
Vince Roy <vincer0117@gmail.com>
2021-08-23 20:45:08 -0400

    upgrade mint

--- packages/templates/src/project/potionx/mix.exs
+++ packages/templates/src/project/potionx/mix.exs
@@ -44,7 +44,7 @@ defmodule <%= appModule %>.MixProject do
       {:ecto_sql, "~> 3.5"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:mint, "~> 1.0"},
+      {:mint, "~> 1.3.0"},
       {:phoenix_ecto, "~> 4.1"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_dashboard, "~> 0.4"},
