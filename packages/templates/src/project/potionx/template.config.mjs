ed9e58ca18ae28c3ae7d4e3c3964ed343bf2dcfd
Vince Roy <vincer0117@gmail.com>
2021-10-10 17:06:31 -0400

    @potionapps/templates: remove vuedx

--- packages/templates/src/project/potionx/template.config.mjs
+++ packages/templates/src/project/potionx/template.config.mjs
@@ -111,7 +111,7 @@ export const getValues = async (context, prompt, initialValues) => {
   }
 
   const collected = await prompt(toCollect);
-  const potionx_version = "0.8.7";
+  const potionx_version = "0.8.8";
 
 
   const timestamp = new Date()
