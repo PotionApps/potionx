aae5cafdf0646f982e3ec3a8a6ad6aa86ee851a1
vince roy <vince@potionapps.com>
2021-12-18 20:52:37 -0500

    package cjs fixes

--- packages/utils/bumpVersion.mjs
+++ packages/utils/bumpVersion.mjs
@@ -3,15 +3,15 @@ import fs from 'fs'
 const packageJson = JSON.parse(fs.readFileSync('./package.json'));
 
 const themes =
-  fs.readdirSync('../ui/src/templates/themes/', { withFileTypes: true })
+  fs.readdirSync('../templates/src/project/potionx/frontend/', { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
 
 themes.forEach(
   theme => {
     const src = JSON.parse(
-      fs.readFileSync(`../ui/src/templates/themes/${theme}/package.json`)
+      fs.readFileSync(`../templates/src/project/potionx/frontend/${theme}/package.json`)
     )
     src.dependencies["@potionapps/utils"] = packageJson.version
-    fs.writeFileSync(`../ui/src/templates/themes/${theme}/package.json`, JSON.stringify(src, null, 2))
+    fs.writeFileSync(`../templates/src/project/potionx/frontend/${theme}/package.json`, JSON.stringify(src, null, 2))
   })
